14331b1c0642e6731e4b930bd710980e51f7e30a
cpmpercussion <cpm@charlesmartin.com.au>
2014-09-27 20:22:40 +1000

    working DMX controller through serial
    changed to DmxSimple library (send only), and figured out DMX shield
    configuration

--- serialDMX/serialDMX.ino
+++ serialDMX/serialDMX.ino
@@ -1,6 +1,24 @@
+#include <DmxSimple.h>
+#define DMX_MASTER_CHANNELS   100
+#define DMX_ENABLE_PIN                2
+#define DMX_TRANSMIT_PIN  4
+#define STEP_INTERVAL           1
+
+int light1 = 1;
+int light2 = 2;
+int light3 = 3;
+int light4 = 4;
+
 void setup() {
-  Serial.begin(9600);
   Serial.println("Ready for some colour strings - receiving HSV!");
+  Serial.begin(9600);
+  DmxSimple.usePin(DMX_TRANSMIT_PIN);
+  DmxSimple.maxChannel(DMX_MASTER_CHANNELS);
+  pinMode(DMX_ENABLE_PIN, OUTPUT);
+  digitalWrite(DMX_ENABLE_PIN, HIGH);
+
+  setLightToRGB(1, 50, 50, 50);
+
 }
 
 void loop() {
@@ -13,20 +31,21 @@ void loop() {
     float red = 0;
     float green = 0;
     float blue = 0;
-    
-    HSVtoRGB(&red,&green,&blue,hue,sat,val);
+
+    HSVtoRGB(&red, &green, &blue, hue, sat, val);
 
     if (Serial.read() == '\n') {
       red = floor(red * 255);
       green = floor(green * 255);
       blue = floor(blue * 255);
-      
       // Set the light going.
+      setLightToRGB(channel, red, green, blue);
+
       Serial.print("Channel ");
       Serial.print(channel);
       Serial.print(": ");
       Serial.print(red);
-      Serial.print(", ");      
+      Serial.print(", ");
       Serial.print(green);
       Serial.print(", ");
       Serial.println(blue);
@@ -34,54 +53,61 @@ void loop() {
   }
 }
 
+void setLightToRGB(int light, int red, int green, int blue) {
+  int baseChannel = ((light - 1) * 4) + 1;
+  DmxSimple.write(baseChannel, 255);
+  DmxSimple.write(baseChannel + 1, red);
+  DmxSimple.write(baseChannel + 2, green);
+  DmxSimple.write(baseChannel + 3, blue);
+}
 
-void HSVtoRGB( float *r, float *g, float *b, float h, float s, float v )
-{
-	int i;
-	float f, p, q, t;
-	if( s == 0 ) {
-		// achromatic (grey)
-		*r = *g = *b = v;
-		return;
-	}
-	h /= 60; // sector 0 to 5
-	i = floor( h );
-	f = h - i; // factorial part of h
-	p = v * ( 1 - s );
-	q = v * ( 1 - s * f );
-	t = v * ( 1 - s * ( 1 - f ) );
-	switch( i ) {
-		case 0:
-			*r = v;
-			*g = t;
-			*b = p;
-			break;
-		case 1:
-			*r = q;
-			*g = v;
-			*b = p;
-			break;
-		case 2:
-			*r = p;
-			*g = v;
-			*b = t;
-			break;
-		case 3:
-			*r = p;
-			*g = q;
-			*b = v;
-			break;
-		case 4:
-			*r = t;
-			*g = p;
-			*b = v;
-			break;
-		default:// case 5:
-			*r = v;
-			*g = p;
-			*b = q;
-			break;
-	}
+
+void HSVtoRGB( float *r, float *g, float *b, float h, float s, float v ) {
+  int i;
+  float f, p, q, t;
+  if ( s == 0 ) {
+    // achromatic (grey)
+    *r = *g = *b = v;
+    return;
+  }
+  h /= 60; // sector 0 to 5
+  i = floor( h );
+  f = h - i; // factorial part of h
+  p = v * ( 1 - s );
+  q = v * ( 1 - s * f );
+  t = v * ( 1 - s * ( 1 - f ) );
+  switch ( i ) {
+    case 0:
+      *r = v;
+      *g = t;
+      *b = p;
+      break;
+    case 1:
+      *r = q;
+      *g = v;
+      *b = p;
+      break;
+    case 2:
+      *r = p;
+      *g = v;
+      *b = t;
+      break;
+    case 3:
+      *r = p;
+      *g = q;
+      *b = v;
+      break;
+    case 4:
+      *r = t;
+      *g = p;
+      *b = v;
+      break;
+    default:// case 5:
+      *r = v;
+      *g = p;
+      *b = q;
+      break;
+  }
 }
 
 
